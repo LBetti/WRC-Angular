3c553d94e5dae77a930001fff0576892670f7385
Luciano Betti <lucianobetti24@gmail.com>
2024-05-17 13:07:59 -0300

    desplegables en banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DesplegableBannerComponent } from './desplegable-banner/desplegable-banner.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DesplegableBannerComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/desplegable-banner/desplegable-banner.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-desplegable-banner',
+  templateUrl: './desplegable-banner.component.html',
+  styleUrls: ['./desplegable-banner.component.css'],
+})
+export class DesplegableBannerComponent {
+  @Input() title: string = 'Dropdown';
+  @Input() items: string[] = [];
+  isOpen: boolean = false;
+
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
+  }
+}
